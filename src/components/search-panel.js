32933cba4d8cd4c5aa6ea94614078f16efa09a7b
Dima Matskello <dimka_matsi@hotmail.com>
2019-01-13 17:06:12 +0300

    finish #28

---
+++ src/components/search-panel.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const SearchPanel = () => <input placeholder="search" />;
+
+export default SearchPanel;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import AppHeader from './components/app-header';
+import SearchPanel from './components/search-panel';
+import TodoList from './components/todo-list';
 
-const AppHeader = () => <h1>My Todo List</h1>;
 
-const SearchPanel = () => <input placeholder="search" />;
 
-const TodoList = () => {
-	return (
-		<ul>
-			<li>Learn React</li>
-			<li>Build Awesome App</li>
-		</ul>
-	);
-};
+
+
 
 const App = () => {
 	return (
