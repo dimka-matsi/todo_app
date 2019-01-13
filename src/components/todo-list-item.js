92b49a94d374a38f7be31ba32692ee513bbfdc2c
Dima Matskello <dimka_matsi@hotmail.com>
2019-01-13 17:19:15 +0300

    finish #29

--- src/components/todo-list-item.js
+++ src/components/todo-list-item.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
-const TodoListItem = () => {
-	return <span>Drink Coffee</span>;
+const TodoListItem = ({label, important = false}) => {
+	const spanStyle = {
+		color: important ? 'tomato' : 'black'
+	};
+	return <span style={spanStyle}>{label}</span>;
 }
 
 export default TodoListItem;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,6 @@ import AppHeader from './components/app-header';
 import SearchPanel from './components/search-panel';
 import TodoList from './components/todo-list';
 
-
-
-
-
-
 const App = () => {
 	return (
 		<div>
